241e825fbab2e9655e81eb56a838e6d548cc9525
Golam Rabbani <knight3rrantt@gmail.com>
2017-03-18 02:38:01 +0600

    update design

--- server.js
+++ server.js
@@ -5,26 +5,29 @@ const port = process.env.PORT || 3000;
 
 FB.setAccessToken('EAACEdEose0cBALCy3bZBdLEL9DFyRP1GNvQv6f89mp7EKlZAQp7NGKuTl4QZAVZAAV0P7Pr5W6102H3JoLN9znaSwZCpU8p0bFAbFWZAlQSEZAWmS37yC9HGdXYIbbXMemPVJZBHxPpOko0Os1CFQY3iRuxL2pwIscZCjsZBKLF2hmSbZCo0U7RrQuCeg2VhrzQnMkZD');
 
-var body = 'My first post using facebook-node-sdk';
-FB.api(
-  '/367870976752456/feed',
-  'GET',
-  {"fields":"message,updated_time"},
-  function(response) {
 
-    s = response.data
+var app = express()
 
-    app.get('/', (req,res)=>{
+app.get('/', (req,res)=>{
+
+  FB.api(
+    '/367870976752456/feed',
+    'GET',
+    {"fields":"message,updated_time"},
+    function(response) {
+      s = response.data
       res.render(__dirname+'/public/root.hbs',{
-        title:'ClassRoom',
+        title:'Alpha Softwarian\'s Latest Post',
         post: s
       })
-    })
+    }
+  );
 
 
 
-  }
-);
-var app = express()
+})
+
+
+
 
 app.listen(port)
