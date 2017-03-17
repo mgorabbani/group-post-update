d71421d77f3407dd00c790d5e26b1de609201aec
Golam Rabbani <knight3rrantt@gmail.com>
2017-03-18 00:48:11 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const hbs = require('hbs');
+const FB = require('fb');
+let s = 'sdf'
+FB.setAccessToken('274539779641078|_8F-vZcJXFQSsrhEZTMWGtDr9r0');
+
+var body = 'My first post using facebook-node-sdk';
+FB.api(
+  '/586400221495560/feed',
+  'GET',
+  {"fields":"message,updated_time"},
+  function(response) {
+
+    s = response.data
+
+    app.get('/', (req,res)=>{
+      res.render(__dirname+'/public/root.hbs',{
+        title:'ClassRoom',
+        post: s
+      })
+    })
+
+
+
+  }
+);
+var app = express()
+
+app.listen(3000)
